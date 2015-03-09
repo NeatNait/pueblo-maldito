a5f82f3b34c67febb25bb99f7b1a53de66ce0f56
alvaropinot <alvaro@neatnait.com>
2015-03-09 01:14:34 +0100

    load image event animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+var src = $('.cover-img').css('background-image');
+var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+
+var img = new Image();
+img.onload = function() {
+    //alert('image loaded');
+    $('.js-wait-for-image').toggleClass('js-wait-for-image animation');
+}
+img.src = url;
+if (img.complete) img.onload();

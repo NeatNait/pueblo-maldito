17a8f91f36c4b17c0d6c8b7cb9ce67a2f2246e7c
Alvaro Pinot <alvaro@neatnait.com>
2015-03-16 13:03:16 +0100

    elasitc scroll and patrocinadores

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var src = $('.cover-img').css('background-image');
 var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
 
@@ -5,17 +7,55 @@ var img = new Image();
 img.onload = function() {
     //alert('image loaded');
     $('.js-wait-for-image').toggleClass('js-wait-for-image animation');
-}
+};
 img.src = url;
-if (img.complete) img.onload();
+if (img.complete){
+  img.onload();
+}
 
 
 
-$('.modal').on('shown.bs.modal', function() {
-        $(this).find('iframe').attr('src','http://entradium.com/entradas/3364.widget')
-    })  
+$(function() {
 
+  $('.modal').on('shown.bs.modal', function() {
+      $(this).find('iframe').attr('src','http://entradium.com/entradas/3364.widget');
+  });
 
+  //smooth scrolling
+  //$('a[href^="#"]').on('click', function (e) {
+  $('a.page-scroll').on('click', function (e) {
+      e.preventDefault();
+
+      var target = this.hash,
+      $target = $(target),
+      marginTop = 80;
+
+      //window.location.hash = target;
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top - marginTop
+      }, 1000, 'swing', function () {
+          //window.location.hash = target;
+          if(history.pushState) {
+            history.pushState(null, null, target);
+          }
+          else {
+            window.location.hash = target;
+          }
+      });
+  });
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+      target: '.navbar-fixed-top'
+  });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
+  });
+
+});
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 /*$(function() {
@@ -28,16 +68,6 @@ $('.modal').on('shown.bs.modal', function() {
     });
 });*/
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 
 
 /**
@@ -46,7 +76,7 @@ $('.navbar-collapse ul li a').click(function() {
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -89,7 +119,7 @@ var cbpAnimatedHeader = (function() {
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
